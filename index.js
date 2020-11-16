039e423d4a652250d5ac7cb8312c6f59e4bee788
purisquiri <mau.dorigo@outlook.es>
2020-11-15 21:04:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(fr){
+  let ride
+  if (fr < 400) {
+    ride = "This one is on me!"
+  }
+  else if (fr>2000&&fr<=2500) {
+    ride = "I will gladly take your thirty bucks."
+  }
+  else if (fr>2500) {
+    ride = 'No can do.'
+  }
+  return ride
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let findCity
+  if (city == "NYC") {
+    findCity = "Ok, sounds good.";
+  }
+  else if (city != "NYC") {
+    findCity = "No go.";
+  }
+  return findCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tips){
+  let charm
+  if (tips == 'generous') {
+    charm = "Thank you so much.";
+  }
+  else if (tips == 'not as generous') {
+    charm = "Thank you.";
+  }
+  else {
+    charm = "Bye.";
+  }
+  return charm
 }
